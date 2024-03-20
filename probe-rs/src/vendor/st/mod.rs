0d20bc10fc6c2d47504ceaa1e2e03c0ac8d6c195
Ambrose Feinstein <ambrose@osdyne.com>
2024-03-20 00:55:38 -0700

    reset sequence for STM32N6
    The secure boot scheme causes the debug port to lock on reset, until the
    ROM code reopens it.  In the interim we only get various SWD errors.

--- probe-rs/src/vendor/st/mod.rs
+++ probe-rs/src/vendor/st/mod.rs
@@ -10,6 +10,7 @@ use crate::{
             stm32_armv7::Stm32Armv7,
             stm32_armv8::Stm32Armv8,
             stm32h7::Stm32h7,
+            stm32n6::Stm32n6,
         },
         Vendor,
     },
@@ -43,6 +44,8 @@ impl Vendor for St {
             DebugSequence::Arm(Stm32Armv7::create())
         } else if chip.name.starts_with("STM32H7") {
             DebugSequence::Arm(Stm32h7::create())
+        } else if chip.name.starts_with("STM32N6") {
+            DebugSequence::Arm(Stm32n6::create())
         } else if chip.name.starts_with("STM32H5")
             || chip.name.starts_with("STM32L5")
             || chip.name.starts_with("STM32U5")
